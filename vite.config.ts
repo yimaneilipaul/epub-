5a6a6bc1b8fcd88d16c8dda89436ac56e0678b19
yimaneilipaul <114293816+yimaneilipaul@users.noreply.github.com>
2025-11-24 21:44:12 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
+  },
   define: {
-    // 兼容代码中使用的 process.env.API_KEY，将其指向 Vite 的环境变量
+    // 兼容代码中使用的 process.env.API_KEY
     'process.env.API_KEY': 'import.meta.env.VITE_API_KEY'
   }
 });
